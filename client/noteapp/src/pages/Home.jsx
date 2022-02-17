9640b1eac03d953d464d772bfbe2c54e0c2d08da
mzafar <m.zafarhayatzada@gmail.com>
2022-02-17 21:26:21 +0500

    Home page created

--- client/noteapp/src/pages/Home.jsx
+++ client/noteapp/src/pages/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { notes } from "./notes";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      <Row className="d-flex justify-content-center">
+        {notes.map((note) => {
+          return (
+            <Col  md={4} xl={3} className='m-3'>
+              <Card border="info">
+                <Card.Header>Header</Card.Header>
+                <Card.Body>
+                  <Card.Title>Info Card Title</Card.Title>
+                  <Card.Text>
+                    {note.note}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
