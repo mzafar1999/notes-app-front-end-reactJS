7843421a0afcc4ed291c70d0de1acfbc6f0185d4
mzafar <m.zafarhayatzada@gmail.com>
2022-02-17 21:49:29 +0500

    form handled code written

--- client/noteapp/src/pages/Register.jsx
+++ client/noteapp/src/pages/Register.jsx
@@ -1,44 +1,87 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Button = styled.button`
-    margin-top:0.8rem
+  margin-top: 0.8rem;
 `;
 
 const Input = styled.input`
-    margin: 0.5rem 0;
-    padding: 0.3rem;
-`
-const Wrapper = styled.div`
-    margin: 2rem auto;
-    width: 400px;
-    display: flex;
-    flex-direction: column;
-`
-const LinkItem = styled(Link)`
-    text-decoration: none;
-    &:hover{
-        color:#909090;
-    }
-    &:focus, &:visited, &:link, &:active {
-        text-decoration: none;
-    }
+  margin: 0.5rem 0;
+  padding: 0.3rem;
 `;
-const Register = () => {
-  return (
-        <Container>
-           <Wrapper>
-           <Input placeholder='Username' />
-           <Input placeholder='Email' />
-            <Input placeholder='Password' type='password' />
-            <Input placeholder='Confirm Password' type='password' />
-            <Button className='btn btn-primary'> Register </Button>
-            <p>Already have account? <LinkItem to={`/login`}>Login</LinkItem> </p>
-           </Wrapper>
-        </Container>
-    )
-}
+const Wrapper = styled.div`
+  margin: 2rem auto;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+`;
+const LinkItem = styled(Link)`
+  text-decoration: none;
+  &:hover {
+    color: #909090;
+  }
+  &:focus,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
 
-export default Register
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPasswowrd, setConfirmPasswowrd] = useState("");
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  const newUser =
+        {
+            username,
+            email,
+            password,
+            confirmPasswowrd,
+          }
+  };
+  return (
+    <Container>
+      <Wrapper>
+        <Input
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <Input
+          placeholder="Email"
+          onChange={(e) => setemail(e.target.value)}
+          value={email}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <Input
+          placeholder="Confirm Password"
+          type="password"
+          onChange={(e) => setConfirmPasswowrd(e.target.value)}
+          value={confirmPasswowrd}
+        />
+        <Button className="btn btn-primary" onClick={handleSubmit}>
+          {" "}
+          Register{" "}
+        </Button>
+        <p>
+          Already have account? <LinkItem to={`/login`}>Login</LinkItem>{" "}
+        </p>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
